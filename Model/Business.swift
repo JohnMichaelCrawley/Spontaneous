ed9f9d6b6961f347cd926f0c4979ba21224c0641
johnmichaelcrawley <johnmichaelcrawley@gmail.com>
2023-01-02 19:34:14 +0000

    Google API in the project can now find random businesses in close proximity to a lat' and long' and display name, type, rating etc in a Google marker.

--- Model/Business.swift
+++ Model/Business.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import GoogleMaps
+import GoogleMapsBase
+import GoogleMapsCore
 
 // USER DEFAULTS
 let USERDEFAULTS = UserDefaults.standard
@@ -20,9 +23,146 @@ let USERDEFAULTS = UserDefaults.standard
  * Keyword = Search term or keyword which should match against all content that google has indexed for this place.
  */
 
-var latitude = USERDEFAULTS.
-var longitude = -6.259353
+let KEY = API().returnAPIKey()
+var latitude = USERDEFAULTS.double(forKey: "userLatitude")
+var longitude = USERDEFAULTS.double(forKey: "userLongitude")
 var radius = 620
+
+var keyword = "cafe"
+
+let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(latitude),\(longitude)&radius=\(radius)&type=\(keyword)&key=\(KEY)";
+
+
+/*
+ BUSINESS:
+ this struct sets, gets
+ data for the business
+ */
+struct Business
+{
+    // Variables for data on the business
+    private var id: String                      // Place ID
+    private var name: String                    // Business Name
+    private var type: [String]                  // Type of Business
+    private var openingHours: [String: Bool]?
+    private var rating: Double                  // Business Rating
+    // Photo data
+  //  private var photos: [Photo]
+    // Variables for location
+    private var address: String                 // Business Address / Vicinity
+    private var lat: Double                     // Latitude for the business
+    private var long: Double                    // longitude for the business
+    
+    init()
+    {
+        self.id = ""
+        self.name = ""
+        self.type = ["", ""]
+        self.openingHours = [:]
+        self.rating = 0.0
+      //  self.photos = []
+        self.address = ""
+        self.lat = 0.0
+        self.long = 0.0
+    }
+    
+    // INIT
+    /*init(id: String, name: String, type: [String], openingHours: [String : Bool]? = nil, rating: Double, photos: [Photo], address: String, lat: Double, long: Double) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.openingHours = openingHours
+        self.rating = rating
+        self.photos = photos
+        self.address = address
+        self.lat = lat
+        self.long = long
+    }
+     */
+     
+    // SETTERS
+    mutating func setBusinessID(ID: String)
+    {
+        self.id = ID
+    }
+    mutating func setBusinessName(name: String)
+    {
+        self.name = name
+    }
+    mutating func setBusinessType(type: [String])
+    {
+        self.type = type
+    }
+    mutating func setBusinessOpeningHours(openingHours: [String: Bool])
+    {
+        self.openingHours = openingHours
+    }
+    mutating func setBusinessRating(rating: Double)
+    {
+        self.rating = rating
+    }
+   /* mutating func setBusinessPhotos(photos: [Photo])
+    {
+        self.photos = photos
+    }
+    */
+    mutating func setBusinessAddress(vicinity: String)
+    {
+        self.address = vicinity
+    }
+    mutating func setBusinessLatitude(lat: Double)
+    {
+        self.lat = lat
+    }
+    mutating func setBusinessLongitude(lng: Double)
+    {
+        self.long = lng
+    }
+    // GETTERS
+    func getBusinessID () -> String
+    {
+        return id
+    }
+    func getBusinessName () -> String
+    {
+        return name
+    }
+    func getBusinessType () -> [String]
+    {
+        return type
+    }
+    func getBusinessOpeningHours () -> [String: Bool]?
+    {
+        return openingHours
+    }
+    func getBusinessRating () -> Double
+    {
+        return rating
+    }
+    /*
+    func getBusinessPhotos () -> [Photo]
+    {
+        return photos
+    }*/
+    func getBusinessAddress () -> String
+    {
+        return address
+    }
+    func getBusinessLat () -> Double
+    {
+        return lat
+    }
+    func getBusinessLong () -> Double
+    {
+        return long
+    }
+    
+}
+
+
+
+
+
 
 /*
 These structs are used to help
@@ -58,4 +198,62 @@ struct Photo: Codable {
     var width: Double
     var photoReference: String
 }
-/*
+
+
+private func getData(from url: String)
+{
+    URLSession.shared.dataTask(with: URL(string: url)! ,completionHandler:
+        { data, task, error in
+        guard let data = data, error == nil else
+        {
+            print("Something went wrong wih getting the data!...")
+            return
+        }
+        // now we have the data we need to do json decoding
+       // var result: SearchResult?
+        let jsonDEcoder = JSONDecoder()
+        /*
+         Try get the random business
+         and store it
+         */
+        do
+        {
+            // Variables decoder to decode JSON
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let root = try decoder.decode(Root.self, from: data)
+            // Get random number from a range of amount of results in JSON
+            let range = root.results.count
+            var index = 0
+            index = Int.random(in: 1..<range)
+            // Assign values to global vars
+            
+            //let business = Business(id: root.results[index].placeId, name: root.results[index].name, type: root.results[index].types, rating: root.results[index].rating, photos: root.results[index].photos, address: root.results[index].vicinity, lat: root.results[index].geometry.location.lat, long: root.results[index].geometry.location.lng)
+            
+            
+            
+            /*
+            businesssName = root.results[index].name
+            businessType = root.results[index].types
+            openingHours = root.results[index].openingHours
+            placeID = root.results[index].placeId
+            vicinity = root.results[index].vicinity
+            rating = root.results[index].rating
+            businessLatitude = root.results[index].geometry.location.lat
+            businessLongitude = root.results[index].geometry.location.lng
+           
+             */
+             // Print values
+           // print("business name: \(businesssName) \nTypes: \(businessType) \nOpening Hours: \(String(describing: openingHours)) \nplace ID: \(placeID) \nAddress: \(vicinity) \nRating: \(rating)")
+           // print("Lat: \(businessLatitude) \nLong: \(businessLongitude)")
+            
+            
+            
+            
+        }
+        catch
+        {
+            print(error)
+        }
+    }).resume()
+}
--- View/Controllers/ViewController.swift
+++ View/Controllers/ViewController.swift
@@ -23,10 +23,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate
 {
     /* VARIABLES */
     @IBOutlet weak var mapView: GMSMapView!             // U.I: Display Google Map's //
-    
     var locationManager = CLLocationManager()           // LocationManager: //
-    
-    
     let USERDEFAULTS = UserDefaults.standard
     
     
@@ -69,8 +66,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate
     {
         mapView.camera = GMSCameraPosition(target: CLLocationCoordinate2D(latitude:  locationManager.location?.coordinate.latitude ?? 0.0, longitude:  locationManager.location?.coordinate.longitude ?? 0.0), zoom: 10, bearing: 0, viewingAngle: 0)
         
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0)
+       // let marker = GMSMarker()
+      //  marker.position = CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0)
         
         /*marker.title = "Title"
          marker.snippet = "Here"
@@ -131,101 +128,110 @@ class ViewController: UIViewController, CLLocationManagerDelegate
      this function is called when the
      button is pressed
      */
+    
+    var b = Business()
+    
+    
     @IBAction func beSpontaneousPressed(_ sender: Any)
     {
+        // Ask user to confirm location
         locationManager.delegate = self
-        
         locationManager.requestWhenInUseAuthorization()
-        // print("Button was pressed!!!")
+        // VAIRABLES FOR SEARCH
+        // USER DEF
+        var lat = 53.347568
+        var long = -6.259353
+        var radius = 620
+        // SEARCH DEF
+        let KEYWORD = "cafe"
+        let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(lat),\(long)&radius=\(radius)&type=\(KEYWORD)&key=\(API().returnAPIKey())";
+        // Google Maps Marker
+        let marker = GMSMarker()
+        // JSON DECODER
+        let JSONDECODER = JSONDecoder()
         
         
+        URLSession.shared.dataTask(with: URL(string: url)! ,completionHandler:
+        { data, task, error in
+            guard let data = data, error == nil else
+            {
+                print("Something went wrong wih getting the data!...")
+                return
+            }
+            
+            do
+            {
+                // Get access to the JSON data
+                JSONDECODER.keyDecodingStrategy = .convertFromSnakeCase
+                let root = try JSONDECODER.decode(Root.self, from: data)
+                // Get random i
+                // Get random number from a range of amount of results in JSON
+                let range = root.results.count
+                var index = 0
+                index = Int.random(in: 1..<range)
+                if index > 1
+                {
+                    // Set data to the business model
+                    self.b.setBusinessID(ID: root.results[index].placeId)
+                    self.b.setBusinessName(name: root.results[index].name)
+                    self.b.setBusinessType(type: root.results[index].types)
+                //    self.b.setBusinessOpeningHours(openingHours: root.results[index].openingHours!)
+                    self.b.setBusinessAddress(vicinity: root.results[index].vicinity)
+                    self.b.setBusinessRating(rating: root.results[index].rating)
+                    self.b.setBusinessLatitude(lat: root.results[index].geometry.location.lat)
+                    self.b.setBusinessLongitude(lng: root.results[index].geometry.location.lng)
+                }
+                else
+                {
+                    print("index is equal to 0")
+                }
+
+                
+            }
+            catch{print(error)}
+            
+        }).resume()
+       
         
-        //    let locationObject = locationManager.location!
-        let latitude = (locationManager.location?.coordinate.latitude ?? 0.0)
-        let longitude = (locationManager.location?.coordinate.longitude ?? 0.0)
+        // SET MARKER UP
+        //  Is Opened?:\(b.getBusinessOpeningHours()) \n
+        marker.title = b.getBusinessName()
+        marker.snippet = """
+         Type/s: \(b.getBusinessType()) \n
+        Rating:\(b.getBusinessRating()) \n
+        """
     
+        //marker.snippet = "\(description) was found with a rating of \(review), the radius it was found in is a radius of \(r) miles"
+        marker.map = mapView
+        mapView.selectedMarker = marker
         
-        // SET / STORE LAT + LONG IN USER DEFAULTS
-        USERDEFAULTS.set(latitude, forKey: "latitude")
-        USERDEFAULTS.set(longitude, forKey: "longitude")
+        print("""
+              NAME=\(b.getBusinessName())\n
+              LAT=\(b.getBusinessLat())\n
+              LNG=\(b.getBusinessLong())
+              """)
         
         
-        
-        
-        // print("Longittude = ", latitude)
-        //  print("Latitude = ", longitude)
-        
-        
+        // zoom = 15
+        let blat =  b.getBusinessLat()
+        let blong =  b.getBusinessLong()
         
         let center = CLLocationCoordinate2D(
-            latitude: locationManager.location?.coordinate.latitude ?? 0.0 ,
-            longitude: locationManager.location?.coordinate.longitude ?? 0.0
+            latitude: blat,
+            longitude: blong
         )
-        
-        //USERDEFAULTS.value(forKey: "searchRadiusFilter"
-        
-        let marker = GMSMarker()
         marker.position = center
         
         
-        
-        
-        // Get radius from USER DEFAULTS
-        let r = (USERDEFAULTS.value(forKey: "searchRadiusFilter")) as! Float
-        
-        let review = "4/5 stars"
-        let description = "this local business does x y and z stuff that is good for bam"
-        
-        
-        
-        marker.title = "Buisness Name"
-        marker.snippet = "\(description) was found with a rating of \(review), the radius it was found in is a radius of \(r) miles"
-        
-        
-        marker.map = mapView
-        
-        mapView.selectedMarker = marker
-        
-        
-        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 15)
+        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude:blat, longitude: blong, zoom: 14)
         self.mapView.animate(to: camera)
         
         
         
         
-        /*
-         Attempt to scrape Google API businesses.
-         Resources used to help:
-         https://akshaydevkate.medium.com/scrape-data-from-google-maps-using-swift-and-google-maps-places-api-911e78c8908
-         */
         
-        /*
-         
-         
-         let radius = 5000
-         let search = "cafe"
-         let file = "JSON"
-         var resultsFound = 0
-         
-         
-         
-         let URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/\(file)?location=\(latitude),\(longitude)&radius=\(radius)&type=\(search)&key=\(API().returnAPIKey())"
-         */
+        
+        
     }
-    
-    
-    
-    
-    
-    private func getData(from url: String){
-        URLSession.shared.dataTask(with: URL(string: url)! ,completionHandler: { data, task, error in
-            guard let data = data, error == nil else {
-                print("Something went wrong")
-                return
-            }})
-     }
-    
-    
-    
     
 }
