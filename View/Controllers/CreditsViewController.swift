e9cb10fcf20ef6f5327de16aa04dd8cbf7821ffc
johnmichaelcrawley <johnmichaelcrawley@gmail.com>
2023-01-03 01:51:35 +0000

    Updated Credits View Controller with a table to display the credits

--- View/Controllers/CreditsViewController.swift
+++ View/Controllers/CreditsViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class CreditsViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var teamTableView: UITableView!
+    
+    let DEVTEAM = ["John Crawley - Developer", "Anu Sahni - Project Supervisor"]
+    let LOCTEAM = ["Chise Negishi - Japanese", ""]
+    let SECTION = ["Development Team", "Localisation Team"]
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
+        teamTableView.delegate = self
+        teamTableView.dataSource = self
+        
+        teamTableView = UITableView(frame: .zero, style: .grouped)
+       
         // Do any additional setup after loading the view.
     }
     
@@ -28,4 +40,81 @@ class CreditsViewController: UIViewController {
 
 }
 
+
+
+extension CreditsViewController: UITableViewDelegate
+{
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let HEADER = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 100))
+        
+        let label = UILabel(frame: CGRect(x: 10, y: 5,
+                                          width: HEADER.frame.size.width - 15,
+                                          height: HEADER.frame.size.height))
+        
+        
+        HEADER.addSubview(label)
+        
+        if section == 0
+        {
+            label.text = "Development Team"
+        }
+        
+        if section == 1
+        {
+            label.text = "Localisation Team"
+        }
+        
+        
+        // label.text = "Header \(section)"
+        return HEADER
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 100
+    }
+    
+    
+}
+
+
+
+extension CreditsViewController: UITableViewDataSource
+{
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 2
+    }
+    
+    
+    
+    // Set the cell amounts to the number of rows in settings array
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return DEVTEAM.count
+    }
+    
+    
+    
+    // Display the items of each table view cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "teamCell", for: indexPath)
+        
+        if indexPath.section == 0
+        {
+            cell.textLabel?.text = DEVTEAM[indexPath.row]
+        }
+        
+        if indexPath.section == 1
+        {
+            cell.textLabel?.text = LOCTEAM[indexPath.row]
+        }
+        
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+        return cell
+    }
+    
+}
 
